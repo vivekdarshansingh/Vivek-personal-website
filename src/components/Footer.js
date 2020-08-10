bf504f8aef4045a40c3dfd3b405d7c2d47305ee9
Vivek Darshan Singh <65931963+vivekdarshansingh@users.noreply.github.com>
2020-08-10 22:19:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Aditya Ketkar</a>  
-        <br/>
-        Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
+         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a>  
         </p>
     </footer>
 )
